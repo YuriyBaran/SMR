7abc4ddd2ca56dbf7a5c8dfa8a16aa9a6f4d5626
Yuriy Baran <103961916+YuriyBaran@users.noreply.github.com>
2022-10-16 21:50:15 +0200

    Add files via upload

--- aframe/dark room/eventos.js
+++ aframe/dark room/eventos.js
@@ -1,9 +1,13 @@
 var cielo = document.querySelector("#cielo");
+var foco = false;
 
+//EVENTO FOCO
 document.addEventListener('keydown', function (e) {
     if (document.querySelector('a-scene').camera.el.object3D.position["z"] <= 0.3)
-    {
-        document.querySelector("#foco").setAttribute('light', "color: red; intensity: 0.5; type: point;distance: 1.5");
-    }
+        if(foco == false){
+            document.querySelector("#foco").setAttribute('light', "color: red; intensity: 0.5; type: point;distance: 1.5");
+            document.querySelector("#sonido_foco").components.sound.playSound();
+            foco = true;
+        }
   });
 
---
+++ aframe/envio de datos/eventos.js
@@ -0,0 +1,10 @@
+var cielo = document.querySelector("#cielo");
+var foco = false;
+
+//EVENTO FOCO
+document.addEventListener('keydown', function (e) {
+        if(foco == false)
+            console.log("SI");
+        
+  });
+
