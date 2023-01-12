3aad37e9b570dc1781af2d33cdc66667845cf700
Yuriy Baran <103961916+YuriyBaran@users.noreply.github.com>
2023-01-12 10:51:48 +0100

    Add files via upload

---
+++ ejercicio 10/botones.js
@@ -0,0 +1,23 @@
+function hatsune() {
+    document.getElementById("frame").src="hatsune.html";
+  }
+
+function corazon() {
+    document.getElementById("frame").src="corazon.html";
+}
+
+function manzana() {
+    document.getElementById("frame").src="manzana.html";
+}
+
+function inicio(){
+    document.getElementById("cuerpo").src="inicio.html";
+}
+
+function figuras(){
+    document.getElementById("cuerpo").src="figuras.html";
+}
+
+function contacto(){
+    document.getElementById("cuerpo").src="contacto.html";
+}
